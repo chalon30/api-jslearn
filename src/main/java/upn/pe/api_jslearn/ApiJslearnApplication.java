e412fa8f7d921f7ced804ffefb7a2ae240d59e66
chalon30 <angelfabiano3030@gmail.com>
2025-11-18 01:43:39 -0500

    prueba admin register

--- src/main/java/upn/pe/api_jslearn/ApiJslearnApplication.java
+++ src/main/java/upn/pe/api_jslearn/ApiJslearnApplication.java
@@ -12,16 +12,20 @@ public class ApiJslearnApplication {
 
 	public static void main(String[] args) {
 
-		// Cargar siempre .env.local
-		io.github.cdimascio.dotenv.Dotenv dotenv = Dotenv.configure()
-				.filename(".env.local")
-				.load();
+		String profile = System.getenv("SPRING_PROFILES_ACTIVE");
 
-		System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
-		System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-		System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		if ("local".equals(profile)) {
+			Dotenv dotenv = Dotenv.configure()
+					.filename(".env.local")
+					.ignoreIfMissing() // <-- evita error si no existe
+					.load();
+
+			System.setProperty("DB_HOST", dotenv.get("DB_HOST"));
+			System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
+			System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+			System.setProperty("DB_USER", dotenv.get("DB_USER"));
+			System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		}
 
 		SpringApplication.run(ApiJslearnApplication.class, args);
 	}
